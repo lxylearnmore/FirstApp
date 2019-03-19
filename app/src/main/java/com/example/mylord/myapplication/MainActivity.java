2bbbad8feee520c5c9f266ef6532de55d3cc6c4e
lxylearnmore <1029401847@qq.com>
2019-03-19 11:09:21 +0800

    2.0 commit

--- app/src/main/java/com/example/mylord/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mylord/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mylord.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String Tag="Android_live_cycle";
@@ -11,6 +14,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(Tag,"onCreate");
+
+        Button btn1=findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LinearActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn2=findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,ConstraintActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn3=findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, TableActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
